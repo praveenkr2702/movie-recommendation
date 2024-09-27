8d73a40f3c139caf7d0eaad02855a4eb1edb6ac2
praveenkr2702 <81421635+praveenkr2702@users.noreply.github.com>
2024-09-27 18:02:26 +0530

    Update MovieService.java

--- movie-recommendation/src/main/java/com/movierecom/service/MovieService.java
+++ movie-recommendation/src/main/java/com/movierecom/service/MovieService.java
@@ -30,123 +30,114 @@ public class MovieService {
 	
 	public String[] promptAi(String storedPrompt)
 	{
-		String finalPrompt="x: "+storedPrompt+"Return , separated categories (mltiple cat pssible) x will fall into, no extra text: Action,Adventure,Animation,Comedy,Crime,Documentary,Drama,Family,Fantasy,History,Horror,Mystery,Romance,Science Fiction,Thriller";
+		String finalPrompt="convert into three comma separated easy keywords (avlbl in tmdb movie website) from: "+storedPrompt;
+		//String finalPrompt="x: "+storedPrompt+"Return , separated categories (mltiple cat pssible) x will fall into, no extra text: Action,Adventure,Animation,Comedy,Crime,Documentary,Drama,Family,Fantasy,History,Horror,Mystery,Romance,Science Fiction,Thriller";
 		
-		String identifiedGenred;
+		String identifiedKeywords;
 		
-		identifiedGenred = chatClient.prompt().user(finalPrompt).call().content();
+		identifiedKeywords = chatClient.prompt().user(finalPrompt).call().content();
 		
-		String[] genreArray = identifiedGenred.split(",\\s*");
-        System.out.println("output >> "+ identifiedGenred);
+		String[] keyWordArray = identifiedKeywords.split(",\\s*");
+        System.out.println("identifiedKeywords >> "+ identifiedKeywords);
         
-        return genreArray;
+        return keyWordArray;
 
 	}
 	
 	
 
-	public List<ArrayList<String>> getGenreId(String storedPrompt) throws IOException 
+	public List<ArrayList<String>> getKeyWordId(String storedPrompt) throws IOException 
 	{
 		
 		
-		String[] genreArray = promptAi(storedPrompt);
+		String[] keyWordArray = promptAi(storedPrompt);
+		
+		String keyWordIds= "";
+		
+		
+		
 		String jsonString ="";
 		JSONObject jsonObject = null;
 		
-		HashMap<Integer,String> genreList = new HashMap<>();
+		int c=0;
 		
-		String genreIds = "";
 		
-		OkHttpClient client = new OkHttpClient();
-
-		Request request = new Request.Builder()
-		  .url("https://api.themoviedb.org/3/genre/movie/list?language=en")
-		  .get()
-		  .addHeader("accept", "application/json")
-		  .addHeader("Authorization", "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyMzMxZjQ1YzkyNWE0MzRkNmJmOTQzNjI1ODQ1ZjE0YyIsIm5iZiI6MTcyNzE4OTI3MS40MjMxNjEsInN1YiI6IjY2ZjI1OTgyZmMwMDk4MzkxNDhkNjQxYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.2eUDtoCGASghQkv5W0pLZ0uR1DebsXqK1kQDqS_O4fQ")
-		  .build();
-
-		Response response = client.newCall(request).execute();
-		
-	
-		
-		if(response.isSuccessful() && response.body()!=null)
+		for(int i=0;i<keyWordArray.length;i++)
 		{
-			
-			jsonString=response.body().string();
-			
-			jsonObject = new JSONObject(jsonString);
-			
-			System.out.println(jsonString); 
-			
-			JSONArray jsonArray = jsonObject.getJSONArray("genres");
-			
-			
-			
-			boolean genreFound=false;
-			
-			
-			for(int j=0;j<genreArray.length;j++)
+			OkHttpClient client = new OkHttpClient();
+			Request request = new Request.Builder()
+			  .url("https://api.themoviedb.org/3/search/keyword?query="+keyWordArray[i]+"&page=1")
+			  .get()
+			  .addHeader("accept", "application/json")
+			  .addHeader("Authorization", "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyMzMxZjQ1YzkyNWE0MzRkNmJmOTQzNjI1ODQ1ZjE0YyIsIm5iZiI6MTcyNzE4OTI3MS40MjMxNjEsInN1YiI6IjY2ZjI1OTgyZmMwMDk4MzkxNDhkNjQxYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.2eUDtoCGASghQkv5W0pLZ0uR1DebsXqK1kQDqS_O4fQ")
+			  .build();
+
+			Response response = client.newCall(request).execute();
+			if(response.isSuccessful() && response.body()!=null)
 			{
 			
-				for(int i=0;i<jsonArray.length();i++)
+				jsonString =response.body().string();
+			
+				jsonObject = new JSONObject(jsonString);
+			
+				int total_results = jsonObject.getInt("total_results");
+			
+				if(total_results==0)
 				{
-					JSONObject tempJsonObj = jsonArray.getJSONObject(i);
-					
-					genreList.put(tempJsonObj.getInt("id"), tempJsonObj.getString("name"));
+					c++;
+					continue;
+				}
+			
+				JSONArray jsonArray=jsonObject.getJSONArray("results");
+			
+				int limit=0;
+				if(jsonArray.length()>=2)
+				{
+					limit=2;
+				}
+				else if(jsonArray.length()==1)
+				{
+					limit=1;
+				}
 				
-					if(tempJsonObj.getString("name").toLowerCase().equals(genreArray[j].toLowerCase()) )
-					{
-						genreFound=true;
-						System.out.println("genre name is "+tempJsonObj.getString("name"));
-						System.out.println("genre id is "+tempJsonObj.getInt("id"));
-						
-						
-						
-						if(j==genreArray.length-1)
-						{
-							genreIds=genreIds+String.valueOf(tempJsonObj.getInt("id"));
-						}
-						else
-						{
-							genreIds=genreIds+String.valueOf(tempJsonObj.getInt("id"))+"%7C";
-						}
-						
-						
-					
-					}
-				
+	
+			
+				for(int j=0;j<limit;j++)
+				{
+					JSONObject tempJsonObj = jsonArray.getJSONObject(j);
+	
+					keyWordIds=keyWordIds+String.valueOf(tempJsonObj.getInt("id"))+"%7C";
 				}
 			}
 			
-			if(!genreFound)
-			{
-				System.out.println("genre not found");
-			}
 		}
 		
-		System.out.println("genreIds >>> "+genreIds);
+		if(c==3)
+		{
+			
+		}
+		//keyWordIds = keyWordIds.substring(0, keyWordIds.lastIndexOf("%7c")) + keyWordIds.substring(keyWordIds.lastIndexOf("%7c") + 3);
+
+		System.out.println("keyWordIds>>> "+keyWordIds);
+		List<ArrayList<String>> x = movieRecommendation(keyWordIds);
 		
-		List<ArrayList<String>> x = movieRecommendation(genreIds, genreList);
-		
-		 return x;
+		return x;
 		
 		
 		
 	}
 
-	public List<ArrayList<String>> movieRecommendation(String genreIds, HashMap<Integer,String> genreList ) throws IOException {
+	public List<ArrayList<String>> movieRecommendation(String keyWordIds) throws IOException {
 		
 		OkHttpClient client = new OkHttpClient();
 
 		Request request = new Request.Builder()
-		  .url("https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc&with_genres="+genreIds)
+		  .url("https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc&with_keywords="+keyWordIds)
 		  .get()
 		  .addHeader("accept", "application/json")
 		  .addHeader("Authorization", "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyMzMxZjQ1YzkyNWE0MzRkNmJmOTQzNjI1ODQ1ZjE0YyIsIm5iZiI6MTcyNzE4OTI3MS40MjMxNjEsInN1YiI6IjY2ZjI1OTgyZmMwMDk4MzkxNDhkNjQxYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.2eUDtoCGASghQkv5W0pLZ0uR1DebsXqK1kQDqS_O4fQ")
 		  .build();
 
-		
 		Response response = client.newCall(request).execute();
 		
 		String movieJsonString = response.body().string();
@@ -156,8 +147,6 @@ public class MovieService {
 		
 		
 		JSONObject movieJsonObject = new JSONObject(movieJsonString);
-		
-		int totalPages=movieJsonObject.getInt("total_pages");
 		
 		JSONArray movieJsonArray = movieJsonObject.getJSONArray("results");
 		
@@ -169,7 +158,7 @@ public class MovieService {
 		{
 			JSONObject tempJsonObj = movieJsonArray.getJSONObject(i);
 			movieNames.add(tempJsonObj.getString("original_title"));
-			posterPaths.add("https://image.tmdb.org/t/p/w185"+tempJsonObj.getString("poster_path"));
+			posterPaths.add("https://image.tmdb.org/t/p/w185"+tempJsonObj.optString("poster_path"));
 		}
 		
 		return Arrays.asList(movieNames,posterPaths);
